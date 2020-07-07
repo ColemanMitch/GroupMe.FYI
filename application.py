6939b0a135e1546eb328df659e226992663b52e5
ColemanMitch <wmitche3@nd.edu>
2020-07-06 21:44:47 -0500

    make it better pt 2

--- application.py
+++ application.py
@@ -139,7 +139,7 @@ def analyze():
 
 if __name__ == "__main__":
     plt.switch_backend('Agg')
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 
